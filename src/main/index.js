be1b3e58ee91898f267a6ac39f33c6e9ad83646a
Сергей Мальчиц <downace@users.noreply.github.com>
2019-07-10 17:48:04 +0900

    Minor fixes, version bump

--- src/main/index.js
+++ src/main/index.js
@@ -150,6 +150,9 @@ let webContents;
 
 expressApp.use(function(req, res, next) {
     res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Headers', [
+        'Content-Type',
+    ].join(','));
     next();
 });
 expressApp.use(bodyParser.urlencoded());
