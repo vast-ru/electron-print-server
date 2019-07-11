733070d1f3354e82adad7b9abdad1c68d354467b
Сергей Мальчиц <downace@users.noreply.github.com>
2019-07-11 11:36:49 +0900

    Build artifact naming improvement, combined x32 + x64 Windows build

--- electron-builder.js
+++ electron-builder.js
@@ -1,22 +1,32 @@
 module.exports = () => {
+    const artifactName = process.env.BUILD_NUMBER
+        ? '${productName}-${version}.${env.BUILD_NUMBER}-${os}-${arch}.${ext}'
+        : '${productName}-${version}-${platform}-${os}.${ext}';
     return {
-        productName: "Electron print server",
-        nsis       : {
-            oneClick: false
+        productName : "Electron print server",
+        nsis        : {
+            oneClick: false,
         },
-        win        : {
+        artifactName: artifactName,
+        win         : {
             target        : [
-                "portable",
-                "nsis"
+                {
+                    target: "portable",
+                    arch  : ['ia32', 'x64'],
+                },
+                {
+                    target: "nsis",
+                    arch  : ['ia32', 'x64'],
+                },
             ],
-            extraResources: "./external/win32/${arch}"
+            extraResources: "./external/win32/${arch}",
         },
-        linux      : {
+        linux       : {
             target  : [
                 "tar.gz",
                 "deb"
             ],
-            category: "Printing"
-        }
+            category: "Printing",
+        },
     };
 };
